3d19d25f3aaeab21321983d5853a369573a59090
yudamhndra <mahendrayudapradana@mail.ugm.ac.id>
2023-10-20 14:38:21 +0700

    add:requirements.txt

--- plants/models.py
+++ plants/models.py
@@ -15,3 +15,16 @@ class Plant(models.Model):
     def __str__(self):
         return f"{self.plant_name}/{self.condition}/{self.disease}"
 
+# detection
+class PlantDetection(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plant_img = models.ImageField(upload_to='plants/images')
+    plant_name = models.CharField(max_length=255)
+    condition = models.CharField(max_length=255)
+    disease = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.plant_name}/{self.condition}/{self.disease}"
--- plants/urls.py
+++ plants/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
-    path('plants/<int:plant_id>/image/', views.get_plant_image, name='get_plant_image'),
-    path('plants/create/', views.create_plant, name='create_plant'),
-    path('plants/<int:plant_id>/update/', views.update_plant, name='update_plant'),
-    path('plants/<int:plant_id>/delete/', views.delete_plant, name='delete_plant'),
+    path('plants/<int:plant_id>/image/', login_required(views.get_plant_image), name='get_plant_image'),
+    path('plants/create/', login_required(views.create_plant), name='create_plant'),
+    path('plants/<int:plant_id>/update/', login_required(views.update_plant), name='update_plant'),
+    path('plants/<int:plant_id>/delete/', login_required(views.delete_plant), name='delete_plant'),
 ]
